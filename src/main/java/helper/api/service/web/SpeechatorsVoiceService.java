98944bbb8c4e1ff322c59e9d7a81a46b9a472e3a
skrasilnikov <skrasilnikov@igatec.com>
2023-07-23 12:50:59 +0300

    Скачивание файлов в папку voices.

--- src/main/java/helper/api/service/web/SpeechatorsVoiceService.java
+++ src/main/java/helper/api/service/web/SpeechatorsVoiceService.java
@@ -11,9 +11,11 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +27,8 @@ localhost:8081/api/voice?lang=suomi&word=maailma
 public class SpeechatorsVoiceService implements VoiceFetcher {
     private final String baseURL = "https://speechactors.com/text-to-speech/";
 
+    String downloadFilePath = new File("voices").getAbsolutePath();
+
     @Override
     public void getSound(String language, String word) {
         Map<String, String> languageToLinkMap = new HashMap<>() {{
@@ -32,14 +36,20 @@ public class SpeechatorsVoiceService implements VoiceFetcher {
             this.put("русский", "russian-russia");
             this.put("suomi", "finnish-finland");
             this.put("deutsch", "german-germany");
-            this.put("française", "french-france");
+            this.put("français", "french-france");
             this.put("español", "spanish-spain");
             this.put("italiano", "italian-italy");
         }};
-
         String url = baseURL + languageToLinkMap.get(language);
 
         ChromeOptions options = new ChromeOptions();
+        Map<String, Object> preferences = new HashMap<>() {{
+            this.put("profile.default_content_settings.popups", 0);
+            this.put("download.default_directory", downloadFilePath);
+            this.put("profile.default_content_setting_values.automatic_downloads", 1);
+        }};
+        options.setExperimentalOption("prefs", preferences);
+
         options.addArguments("headless");
 
         WebDriver driver = new ChromeDriver(options);
@@ -47,7 +57,6 @@ public class SpeechatorsVoiceService implements VoiceFetcher {
 
         By inputTextBy = By.id("ms-input-text");
         WebElement textInput = driver.findElement(inputTextBy);
-//        log.info("Найден элемент ввода текста: '{}'.", textInput.getTagName());
         textInput.sendKeys(word);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.of(10L, ChronoUnit.SECONDS));
