3d5ebd6eeeeffe93b6d673cf4139566c64ea352d
skrasilnikov <skrasilnikov@igatec.com>
2023-10-14 23:24:10 +0300

    Загрузка языков через скрипт.

---
+++ src/main/resources/static/js/sounding.js
@@ -0,0 +1,125 @@
+let w1, w2, l1, l2, m, words_env;
+let r;
+let wl1, wl2;
+let gv1, gv2;   // Кнопки загрузки звука
+
+$(document).ready(function() {
+    loadLanguages();
+
+    w1 = $('#word1');
+    w2 = $('#word2');
+    l1 = $('#lang1');
+    l2 = $('#lang2');
+    m = $('#message');
+    words_env = $('div.dict_article');
+    r = $('#result_icon')
+    wl1 = $('#word_list_1');
+    wl2 = $('#word_list_2');
+    gv1 = $('#getVoice1');
+    gv2 = $('#getVoice2');
+
+    l1.on('input propertychange', function () {
+        validateFields();
+    });
+    l2.on('input propertychange', function () {
+        validateFields();
+    });
+    w1.on('input propertychange', function () {
+        validateFields();
+        findWordAndTranslation(l1.val(), w1.val(), l2.val());
+    });
+    w2.on('input propertychange', function () {
+        validateFields();
+        findWord(l2.val(), w2.val());
+    });
+
+    $('#ok_btn').on('click', function () {
+        collectAndSendData();
+    });
+
+    w1.keypress(function (event){
+        if (13 === event.which) {
+            collectAndSendData();
+        }
+    });
+    w2.keypress(function (event){
+        if (13 === event.which) {
+            collectAndSendData();
+        }
+    });
+
+    $('#swap_button').on('click', function () {
+        swapWords();
+    });
+
+    $('#getVoice1').on('click', function() {
+        getVoice($('#lang1').val(), $('#word1').val())
+    });
+    $('#getVoice2').on('click', function() {
+        getVoice($('#lang2').val(), $('#word2').val())
+    });
+});
+
+function setLoadVoiceEnabled(enabled) {
+    if(enabled) {
+        gv1.removeClass("disabled");
+        gv2.removeClass("disabled");
+    } else {
+        gv1.addClass("disabled");
+        gv2.addClass("disabled");
+    }
+}
+
+function loadLanguages() {
+    console.log("Загрузка языков");
+    $.ajax({
+        type: 'GET',
+        url: 'api/language',
+        contentType:"application/json; charset=utf-8",
+        success: function (data) {
+            $.each(data.list, function (index, value) {
+                if(data.usedLanguages[0] === value.id) {
+                    l1.val(value.name)
+                }
+                if(data.usedLanguages[1] === value.id) {
+                    l2.val(value.name)
+                }
+                $('#lang_list_1').append($('<option />').val(value.name).text(value.name));
+                $('#lang_list_2').append($('<option />').val(value.name).text(value.name));
+            });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            m.text('Не удалось получить список языков').fadeIn(10);
+            setTimeout(function() {
+                m.fadeOut(3000);
+            }, 5000);
+        }
+    });
+}
+
+function getVoice(lang, word) {
+    console.log("Это getVoice, lang: " + lang + ", word: " + word);
+
+//    $.ajax({
+//        type: 'GET',
+//        url: 'api/voice'
+//    });
+    $.ajax({
+        type: 'GET',
+        url: 'api/voice',
+        data: {
+            lang: lang,
+            word: word
+        },
+        contentType:"application/json; charset=utf-8",
+        success: function (data) {
+            console.log("Озвучивание успешно получено.")
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            m.text('Не удалось озвучивание для слова " + word + "').fadeIn(10);
+            setTimeout(function() {
+                m.fadeOut(3000);
+            }, 5000);
+        }
+    });
+}
