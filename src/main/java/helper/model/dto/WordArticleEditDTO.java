0ca1d0447b4f05ded59e113336720b02506eb020
Сергей Красильников <roux-job@mail.ru>
2024-05-25 15:56:03 +0300

    ToString

--- src/main/java/helper/model/dto/WordArticleEditDTO.java
+++ src/main/java/helper/model/dto/WordArticleEditDTO.java
@@ -3,10 +3,12 @@ package helper.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class WordArticleEditDTO {
     private String lang1, lang2;
     private String word1, word2;
